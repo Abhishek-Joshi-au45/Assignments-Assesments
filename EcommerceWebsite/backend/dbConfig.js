50262b9f1855f91aa9973755a35eaea5043ffb21
Abhishek joshi <joshiabhishek2030@gmail.com>
2023-03-18 17:15:08 +0530

    superfan studio assignmnet ecommerce website

---
+++ EcommerceWebsite/backend/dbConfig.js
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose';
+
+
+async function initDB() {
+    try {
+      
+      await mongoose.connect(process.env.MONGODB_URL, { dbName:"Shopaholic" })
+      console.log("Connected to DB Successfully")
+    } catch (err) {
+       // console.log(err)
+      console.log("Error connecting to DB")
+    }
+  }
+
+  export default initDB
